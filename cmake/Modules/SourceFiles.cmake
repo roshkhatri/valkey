29b9a30c990d1342edf734a352bf1f20faa235a8
Sarthak Aggarwal <sarthagg@amazon.com>
2026-04-02 09:33:28 -0700

    improvements in interface

--- cmake/Modules/SourceFiles.cmake
+++ cmake/Modules/SourceFiles.cmake
@@ -122,6 +122,7 @@ set(VALKEY_SERVER_SRCS
     ${CMAKE_SOURCE_DIR}/src/fifo.c
     ${CMAKE_SOURCE_DIR}/src/mutexqueue.c
     ${CMAKE_SOURCE_DIR}/src/compression.c
+    ${CMAKE_SOURCE_DIR}/src/compression_lz4.c
     ${CMAKE_SOURCE_DIR}/src/compression_stream.c
     ${CMAKE_SOURCE_DIR}/src/compression_rio.c)
 
